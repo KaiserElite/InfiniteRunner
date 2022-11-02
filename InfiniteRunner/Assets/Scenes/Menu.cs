bf6303607c3fd1a8b8d66be66114077de6a1a202
Twaddleb <115522051+Twaddleb@users.noreply.github.com>
2022-11-02 11:42:57 -0800

    Add files via upload

---
+++ InfiniteRunner/Assets/Scenes/Menu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Open()
+    {
+        //Shows the menu
+        gameObject.SetActive(true);
+
+
+    }
+    //A method that we want to hook up to when the restart button gets pressed
+    public void Restart()
+    {
+        //Add code here to restart the game
+        SceneManager.LoadScene(0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
