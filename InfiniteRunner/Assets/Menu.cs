14d8e4dc7d6c87d920bc412f4f7fe17d25536a32
Twaddleb <britnp2@uci.edu>
2022-11-03 21:32:47 -0800

    Menu and pause screen added
    Feel free to make any changes, notify me if something doesnt work
    The options button doesn't work, because I'm not sure if its needed or if it can contain anything useful

--- InfiniteRunner/Assets/Menu.cs
+++ InfiniteRunner/Assets/Menu.cs
@@ -6,10 +6,6 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
     private void Open()
     {
         //Shows the menu
